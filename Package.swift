448791489e4c7ae8a9765155f972d15523517c29
shaihi <shaihi@icloud.com>
2026-03-21 22:41:51 +0200

    fix: embed Info.plist via linker flags to provide bundle identifier for SwiftUI app

--- Package.swift
+++ Package.swift
@@ -13,7 +13,15 @@ let package = Package(
         .executableTarget(
             name: "PhotoCullApp",
             dependencies: ["PhotoCullCore"],
-            path: "Sources/PhotoCullApp"
+            path: "Sources/PhotoCullApp",
+            linkerSettings: [
+                .unsafeFlags([
+                    "-Xlinker", "-sectcreate",
+                    "-Xlinker", "__TEXT",
+                    "-Xlinker", "__info_plist",
+                    "-Xlinker", "Sources/PhotoCullApp/Info.plist"
+                ])
+            ]
         ),
         .testTarget(
             name: "PhotoCullCoreTests",
